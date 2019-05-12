2abfabc912f5935c7f12747397497729b4591158
JoJeongJin <apjojeongjin@gmail.com>
2019-05-12 18:18:06 +0900

    행렬값 저장 완료 - 이제 OPENGL 구현중

--- run_webcam.py
+++ run_webcam.py
@@ -4,9 +4,15 @@ import time
 
 import cv2
 import numpy as np
+#import Opengl_test
 
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
+
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 logger = logging.getLogger('TfPoseEstimator-WebCam')
 logger.setLevel(logging.DEBUG)
@@ -18,6 +24,11 @@ logger.addHandler(ch)
 
 fps_time = 0
 
+def drawCube(vertices):
+    glBegin(GL_LINES)
+    for vertex in vertices:
+            glVertex3fv(vertex)
+    glEnd()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation realtime webcam')
@@ -43,8 +54,22 @@ if __name__ == '__main__':
     cam = cv2.VideoCapture(args.camera)
     ret_val, image = cam.read()
     logger.info('cam image=%dx%d' % (image.shape[1], image.shape[0]))
+    # Opengl_test.myOpenGL()
+
+    #이 부분이 렌더링 시작을 위한 초기화 부분
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -9)
 
     while True:
+
+        ##opengl
+        # Opengl_test.myOpenGL()
+        ##opengl_end
+
         ret_val, image = cam.read()
 
         logger.debug('image process+')
@@ -61,19 +86,24 @@ if __name__ == '__main__':
         #             alist.append([i, j])
         # arr = np.array(alist)
 
-        cv2.imshow('tf-pose-estimation result', image)
+        # cv2.imshow('tf-pose-estimation result', image)
+        vertices = []
         only_pose_image = image
         for i in range (0, 480):
             for j in range (0, 640):
                 if (image[i, j, 0] == 255) and (image[i, j, 1] == 255) and (image[i, j, 2] == 255):
+
+                    x_input = float(i) / 195
+                    y_input = float(j) / 162
+                    vertices.append([x_input,y_input,0])
+                    print([x_input,y_input,0])
                     continue
                 else:
                     only_pose_image[i,j, 0] = 0
                     only_pose_image[i,j, 1] = 0
                     only_pose_image[i,j, 2] = 0
 
-
-        #이미지를 이미지 프로세싱을 통해서 254만큼 빼서 값이 1이상 되는 것들만
+        #이미지를 이미지 프로세싱을 통해서 흰색인 것만 남기고 나머지는 까맣게 만든다.
         #행렬에 그 좌표로 넣는다.
 
         #alist.append([i,j,k])
@@ -84,12 +114,22 @@ if __name__ == '__main__':
 
         #선 연결된 부분은 연결된 것으로 처리하는게 좋다.
 
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        drawCube(vertices)
+        pygame.display.flip()
+
         logger.debug('show+')
-        cv2.putText(image,
-                    "FPS: %f" % (1.0 / (time.time() - fps_time)),
-                    (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (0, 255, 0), 2)
-        cv2.imshow('only_pose_image', only_pose_image)
+        # cv2.putText(image,
+        #             "FPS: %f" % (1.0 / (time.time() - fps_time)),
+        #             (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+        #             (0, 255, 0), 2)
+        # cv2.imshow('only_pose_image', only_pose_image)
+        import matplotlib.pyplot as plt
+
+        plt.plot(vertices[0],vertices[1])
+        print("plot 출력")
+        plt.savefig('demo.png')
+
         fps_time = time.time()
         if cv2.waitKey(1) == 27:
             break
